db651e8f564575492ee51cb09cc39e65a29128e8
fabioba <fabiobarbazza@live.it>
2022-02-14 08:59:37 +0100

    feat: parametric test

--- modeldrift.py
+++ modeldrift.py
@@ -48,7 +48,7 @@ def read_data(path):
 
 def check_past_scores(list_scores):
     """
-        Read past scores
+        Check if all the past scores are worse than current score
 
         Args:
             list_scores(list): list scores
@@ -63,9 +63,9 @@ def check_past_scores(list_scores):
         logger.info('min past score: {}'.format(min_past_score))
 
         if min_past_score<newr2:
-            logger.info('current r2 is NOT worse than past scores')
+            logger.info('RAW TEST: current r2 is NOT worse than past scores')
         elif min_past_score>newr2:
-            logger.info('current r2 is worse than past scores')
+            logger.info('RAW TEST: current r2 is worse than past scores')
 
 
     except Exception as err:
@@ -73,8 +73,83 @@ def check_past_scores(list_scores):
         raise
 
 
+def check_parametric_past_scores(list_scores):
+    """
+        Check if all past scores are worse the current one, using parametric test
+
+        Args:
+            list_scores(list): list scores
+    """
+    try:
+        logger.info('START')
+
+
+        # calculate parametric statistics
+        mean_past_scores=np.mean(list_scores)
+        std_past_scores=np.std(list_scores)
+
+        logger.info('standard deviation past scores: {}'.format(std_past_scores))
+
+        # this value is the upper limit of worse scoring value, if the current value is less than this, so there's model drift
+        value_worse_score=mean_past_scores - std_past_scores*2
+
+        logger.info('limit value worse score: {}'.format(value_worse_score))
+
+
+
+        if value_worse_score<newr2:
+            logger.info('PARAMETRIC TEST: current r2 is NOT worse than past scores')
+        elif value_worse_score>newr2:
+            logger.info('PARAMETRIC TEST: current r2 is worse than past scores')
+
+
+    except Exception as err:
+        logger.exception(err)
+        raise
+
+
+
+def check_non_parametric_past_scores(list_scores):
+    """
+        Check if all past scores are worse the current one, using parametric test
+
+        Args:
+            list_scores(list): list scores
+    """
+    try:
+        logger.info('START')
+
+
+        # calculate parametric statistics
+        mean_past_scores=np.mean(list_scores)
+        std_past_scores=np.std(list_scores)
+
+        logger.info('standard deviation past scores: {}'.format(std_past_scores))
+
+        # this value is the upper limit of worse scoring value, if the current value is less than this, so there's model drift
+        value_worse_score=mean_past_scores - std_past_scores*2
+
+        logger.info('limit value worse score: {}'.format(value_worse_score))
+
+
+
+        if value_worse_score<newr2:
+            logger.info('PARAMETRIC TEST: current r2 is NOT worse than past scores')
+        elif value_worse_score>newr2:
+            logger.info('PARAMETRIC TEST: current r2 is worse than past scores')
+
+
+    except Exception as err:
+        logger.exception(err)
+        raise
+
+
+
 if __name__=='__main__':
     LIST_SCORES=read_data('data/previousscores.txt')
 
     check_past_scores(LIST_SCORES)
 
+    check_parametric_past_scores(LIST_SCORES)
+
+    check_non_parametric_past_scores(LIST_SCORES)
